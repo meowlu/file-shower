ae27c359cb47d64f66d6b578f506148191b7a39e
meowlu <159621267+meowlu@users.noreply.github.com>
2024-08-05 17:30:13 +0800

    Update app_catch.py

--- app_catch.py
+++ app_catch.py
@@ -2,12 +2,21 @@ import os
 import tkinter as tk
 from tkinter import messagebox
 from set import *
+def open_folder(path):
+    import platform
+    os_name=platform.system()
+    if os_name == "Windows":
+        os.startfile(path)
+    elif os_name == "Darwin":
+        os.system(f'open "{path}"')
+    elif os_name == "Linux":
+        os.system(f'xdg-open "{path}"')
 def write_folder_contents(folder_path,output,indent_level=0):
     if not os.path.exists(folder_path):
             show_error(f"Error: The folder '{folder_path}' does not exist.\nPlease check the path and try again.")
             return
     output.tag_configure("file_tag", foreground="black")
-    output.tag_configure("folder_tag", foreground="blue")
+    output.tag_configure("folder_tag", foreground="blue", underline=True)
     try:
         for subdir, dirs, files in os.walk(folder_path):
             if subdir == folder_path:
@@ -16,8 +25,16 @@ def write_folder_contents(folder_path,output,indent_level=0):
                 for file in files:
                     output.insert("end", f"{'  ' * indent_level}file: {file}\n",'file_tag')
                 for dir in dirs:
+                    output.insert("end", f"{'  ' * indent_level}")
                     sub_subdir = os.path.join(subdir, dir)
-                    output.insert("end", f"{'  ' * indent_level}folder: {dir}\n","folder_tag")
+                    start_index = output.index(f"end-1c linestart+{indent_level * 2} chars")
+                    output.insert("end", f"folder: {dir}")
+                    end_index = output.index("end-1c lineend")
+                    output.insert("end",'\n')
+                    output.tag_add("folder_tag", start_index, end_index)
+                    output.tag_bind('folder_tag', "<Button-1>", lambda e, path=sub_subdir: open_folder(path))
+                    output.tag_bind('folder_tag', "<Enter>", lambda e: output.config(cursor="hand2"))
+                    output.tag_bind('folder_tag', "<Leave>", lambda e: output.config(cursor=""))
                     write_folder_contents(sub_subdir, output, indent_level + 2)
             break
     except PermissionError as e:
